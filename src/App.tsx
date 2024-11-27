3d5b942a243d877bbb286e5562b508b5a7b0cf38
egzoniar <arifajegzon.ea@gmail.com>
2024-11-27 15:36:00 +0100

    Hotjar setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import 'reactflow/dist/base.css';
-import AppLayout from './pages/AppLayout';
+import React, { useEffect } from "react";
+import { hotjar } from "react-hotjar";
+import "reactflow/dist/base.css";
+import AppLayout from "./pages/AppLayout";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    hotjar.initialize({ id: 5223093, sv: 6 });
+  }, []);
 
   return (
-    <div className='h-screen bg-[#FAFAFA]'>
+    <div className="h-screen bg-[#FAFAFA]">
       <AppLayout />
     </div>
   );
