67676f471f4794dc38d0b9e5dfab49d937a23530
egzoniar <arifajegzon.ea@gmail.com>
2024-11-28 11:37:39 +0100

    Microsoft Clarity setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
-import { hotjar } from "react-hotjar";
-import "reactflow/dist/base.css";
 import AppLayout from "./pages/AppLayout";
+import Clarity from "@microsoft/clarity";
+import "reactflow/dist/base.css";
 
 const App: React.FC = () => {
   useEffect(() => {
-    hotjar.initialize({ id: 5223093, sv: 6 });
+    Clarity.init("p5kwy5c7r3");
   }, []);
 
   return (
