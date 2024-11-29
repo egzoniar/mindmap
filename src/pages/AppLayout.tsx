5abca8cee302903a39eb724dc2f76502b9e48f7c
egzoniar <arifajegzon.ea@gmail.com>
2024-11-29 09:06:03 +0100

    Small fix.

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,15 +1,18 @@
-import SidebarContainer from "src/components/sidebar/SidebarContainer";
+import SidebarContainer from "src/components/map-sidebar/SidebarContainer";
 import MindmapPage from "./MindmapPage";
-
+import { CustomSidebarProvider } from "./CustomSidebarProvider";
+import { SidebarTrigger } from "src/shadcn-components/ui/sidebar";
+import { AppSidebar } from "./AppSidebar";
 
 const AppLayout = () => {
-
   return (
     <div className="h-screen">
       <SidebarContainer />
       <MindmapPage />
     </div>
-  )
+    // <CustomSidebarProvider>
+    // </CustomSidebarProvider>
+  );
 };
 
 export default AppLayout;
